e6d44b8b5fa4d0568bf92bbff694ef4dd8071ce2
shineunji <dmswlckato@gmail.com>
2020-06-23 18:59:03 +0900

    docs:react JSX

---
+++ study06/0622/hello-react/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  const name = "리액트";
+  return (
+    <div>
+      {name === "리액트" ? <h1>리액트입니다.</h1> : <h1>리액트 아닙니다.</h1>}
+    </div>
+  );
+}
+
+export default App;
