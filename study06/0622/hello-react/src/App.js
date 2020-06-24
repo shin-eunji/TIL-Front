3c40c1074ac73149b678dda891bfd5c0210f35f7
shineunji <dmswlckato@gmail.com>
2020-06-24 11:04:47 +0900

    JSX 문법 및 컴포넌트

--- study06/0622/hello-react/src/App.js
+++ study06/0622/hello-react/src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
 
 function App() {
-  const name = "리액트";
+  const name = "리액트 입니다";
+  const style = {
+    backgroundColor: "black",
+    color: "aqua",
+    fontSize: "48px",
+    fontWeight: "bold",
+    padding: 16,
+  };
   return (
-    <div>
-      {name === "리액트" ? <h1>리액트입니다.</h1> : <h1>리액트 아닙니다.</h1>}
+    <div style={style}>
+      {/* {name === "리액트" ? <h1>리액트입니다.</h1> : <h1>리액트 아닙니다.</h1>} */}
+      {/* {name === "리액트" ? <h1>리액트입니다.</h1> : null} */}
+      {name}
+      <input />
     </div>
   );
 }
