2ff507e4906addb10c29bf6989ee55e8ebac1b8e
shineunji <dmswlckato@gmail.com>
2020-07-04 12:39:31 +0900

    docs: Lexical Environment

--- javascript-ch3/script.js
+++ javascript-ch3/script.js
@@ -1,15 +1,41 @@
-var add1 = (function () {
-  var a = 10;
-  return function (x, y) {
-    return x + y + a;
-  };
-})();
+// let add1 = (function () {
+//   let a = 10;
+//   return function (x, y) {
+//     return x + y + a;
+//   };
+// })();
 
-console.log(add1(1, 2));
+// console.log(add1(1, 2));
 
-var add2 = (function () {
-  var a = 10;
-  return new Function("x", "y", "return x + y + a");
-})();
+// let add2 = (function () {
+//   let a = 10;
+//   return new Function("x", "y", "return x + y + a");
+// })();
 
-console.log(add2(1, 2));
+// console.log(add2(1, 2));
+
+let a = 1;
+
+if (true) {
+  let b = 2;
+  if (true) {
+    let c = 3;
+  }
+}
+
+function foo() {
+  let d = 4;
+
+  function bar() {
+    let e = 5;
+  }
+
+  bar();
+}
+foo();
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
