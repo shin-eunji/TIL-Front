1361e0ee204c11a4d06165ee6491d4cf6770ac01
shineunji <dmswlckato@gmail.com>
2020-07-06 15:52:01 +0900

    docs: Execution Context

--- javascript-0706/6-1.js
+++ javascript-0706/6-1.js
@@ -1,33 +1,18 @@
-// function foo() {
-//   x = 10;
-// }
+// 실행 컨텍스트의 역할
+//전역 변수
+const x = 1;
+const y = 2;
 
-// foo();
-
-// console.log(x); // 10
-
-console.log(this); // window
-
-function square(number) {
-  console.log(this);
-  return number * number; // 4
+// 함수 정의
+function foo(a) {
+  // 지역 변수
+  const x = 10;
+  const y = 20;
+  // 메소드 호출
+  console.log(a + x + y); // 130 (100+10+20)
 }
+// 함수 호출
+foo(100);
 
-square(2);
-
-const person = {
-  name: "shin",
-  getName() {
-    console.log(this);
-    return this.name;
-  },
-};
-
-console.log(person.getName()); // 'shin'
-
-function Person(name) {
-  this.name = name;
-  console.log(this); //Person {name: 'kim'}
-}
-
-const me = new Person("kim");
+// 메소드 호출
+console.log(x + y); // 3
---
+++ javascript-0706/6-2.js
@@ -0,0 +1,13 @@
+// 실행 컨텍스트 스택
+const x = 1;
+function foo() {
+  const y = 2;
+
+  function bar() {
+    const z = 3;
+    console.log(x + y + z); //
+  }
+  bar();
+}
+
+foo();
