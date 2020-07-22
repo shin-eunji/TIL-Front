91f99a564964f1e3305b733d4b62a5a51fb64323
shineunji <dmswlckato@gmail.com>
2020-07-22 23:46:52 +0900

    javascript 0722

---
+++ javascript-0722/index.js
@@ -0,0 +1,20 @@
+const title = document.querySelector("#title");
+
+document.title = "shineunji";
+
+const CLICKED = "clicked";
+
+function handleClick() {
+  //   const hasClass = title.classList.contains(CLICKED);
+  //   if (hasClass) {
+  //     title.classList.remove(CLICKED);
+  //   } else {
+  //     title.classList.add(CLICKED);
+  //   }
+  title.classList.toggle(CLICKED);
+}
+
+function init() {
+  title.addEventListener("click", handleClick);
+}
+init();
