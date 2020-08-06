bd6013eb4f6b13cdb8371de1a55c046c58e58b47
shineunji <dmswlckato@gmail.com>
2020-08-06 16:39:28 +0900

    docs: function

--- javascript-0806/script.js
+++ javascript-0806/script.js
@@ -1,3 +1,21 @@
-// headingTag 태그
-let headingTag = document.querySelector("h1");
-headingTag.textContent = "Hello World!!!";
+// // headingTag 태그
+// let headingTag = document.querySelector("h1");
+// headingTag.textContent = "Hello World!!!";
+
+// 조건문
+// let iceCream = "chocolatel";
+// if (iceCream == "chocolate") {
+//   console.log("아이스크림이 맞습니다.");
+// } else {
+//   console.log("아이스크림이 아닙니다.");
+// }
+
+// 함수
+function multipy(num1, num2) {
+  let result = num1 * num2;
+  return result;
+}
+
+console.log(multipy(10, 20));
+console.log(multipy(15, 19));
+console.log(multipy(31, 53));
