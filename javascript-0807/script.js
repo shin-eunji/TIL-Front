8372b9411ec445354d2a1bee4b7c4500b29a21be
shineunji <dmswlckato@gmail.com>
2020-08-07 11:47:29 +0900

    docs: Object Oriented Programming, OOP

--- javascript-0807/script.js
+++ javascript-0807/script.js
@@ -27,3 +27,32 @@
 // };
 
 // console.log(grades.kim);
+
+// let arr = ["a", "b", "c"];
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// let grades = {
+//   kim: 1,
+//   lee: 2,
+//   shin: 3,
+// };
+
+// for (key in grades) {
+//   console.log(`key: ${key}, value: ${grades[key]}`);
+//   console.log(key);
+// }
+
+let grades = {
+  list: {
+    kim: 1,
+    lee: 2,
+    shin: 3,
+  },
+  show: function () {
+    alert(this);
+  },
+};
+grades["show"]();
