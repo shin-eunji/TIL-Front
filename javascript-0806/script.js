6c92ad016aa98a5b5ae19bdf148f9346b8ae20ac
shineunji <dmswlckato@gmail.com>
2020-08-07 11:22:22 +0900

    docs: 0807

--- javascript-0806/script.js
+++ javascript-0806/script.js
@@ -33,3 +33,42 @@
 // });
 
 // - mousedown event
+let isDrawing = false;
+let x = 0;
+let y = 0;
+
+const myPics = document.getElementById("Canvas");
+const context = myPics.getContext("2d");
+
+myPics.addEventListener("mousedown", (e) => {
+  x = e.offsetX;
+  y = e.offsetY;
+  isDrawing = true;
+});
+
+myPics.addEventListener("mousemove", (e) => {
+  if (isDrawing === true) {
+    drawLine(context, x, y, e.offsetX, e.offsetY);
+    x = e.offsetX;
+    y = e.offsetY;
+  }
+});
+
+window.addEventListener("mouseup", (e) => {
+  if (isDrawing === true) {
+    drawLine(context, x, y, e.offsetX, e.offsetY);
+    x = 0;
+    y = 0;
+    isDrawing = false;
+  }
+});
+
+function drawLine(context, x1, y1, x2, y2) {
+  context.beginPath();
+  context.strokeStyle = "red";
+  context.lineWidth = 2;
+  context.moveTo(x1, y1);
+  context.lineTo(x2, y2);
+  context.stroke();
+  context.closePath();
+}
