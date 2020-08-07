edbca8366a67551d7d1d18a7e58103c2d38f5097
shineunji <dmswlckato@gmail.com>
2020-08-07 11:27:08 +0900

    docs: function

--- javascript-0807/script.js
+++ javascript-0807/script.js
@@ -1,6 +1,21 @@
-let i = 0;
+// let i = 0;
 
-while (i < 10) {
-  document.write("Coding everybody ");
-  i++;
-}
+// while (i < 10) {
+//   document.write("Coding everybody ");
+//   i++;
+// }
+
+// function num() {
+//   i = 0;
+//   while (i < 10) {
+//     document.write(i);
+//     i += 1;
+//   }
+// }
+
+// num();
+// num();
+// num();
+// num();
+// num();
+// num();
