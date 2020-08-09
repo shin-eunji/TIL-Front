6591d25da7a96c4c462a8baad11c160efeb77b7f
shineunji <dmswlckato@gmail.com>
2020-08-09 20:15:41 +0900

    family Object

--- javascript-0809/script.js
+++ javascript-0809/script.js
@@ -1,7 +1,13 @@
-let family = {
-    'address': 'seoul'
-    member: {},
-    addFamily: function() {
-        this.member[role]
-    }
-}
+var family = {
+  address: "seoul",
+  members: {},
+  addFamily: function (age, name, role) {
+    this.member[role] = {
+      age: age,
+      name: name,
+    };
+  },
+  getHeadcount: function () {
+    return Object.keys(this.members).length;
+  },
+};
