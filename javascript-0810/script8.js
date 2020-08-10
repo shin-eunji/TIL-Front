05741c07a1ed6aa85c6b9cf34ae305fe3e03e6b2
shineunji <dmswlckato@gmail.com>
2020-08-10 14:16:35 +0900

    docs: let

--- javascript-0810/script8.js
+++ javascript-0810/script8.js
@@ -11,4 +11,16 @@ function print() {
 }
 
 print();
-console.log(b); // ReferenceError
+// console.log(b); // ReferenceError
+
+// 호이스팅
+hello();
+function hello() {
+  console.log("안녕하세요.");
+}
+
+// 함수 표현식을 통해 변수에 할당하여 호출
+hello2();
+let hello2 = function () {
+  console.log("안녕하세요.");
+};
---
+++ javascript-0810/script9.js
@@ -0,0 +1,8 @@
+// let, const
+if (true) {
+  var functionScopeValue = "global";
+  let blockScopeValue = "local";
+}
+
+console.log(functionScopeValue); // global
+console.log(blockScopeValue); // ReferenceError
